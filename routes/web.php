c1ebfec2216d5f54ee11cde6d716af13e0f046d5
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-02-23 15:04:05 +0700

    route prac

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Welcome';
 });
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
+   Route::get('/about', function () {
+    return 'Name : Lenka Melinda Florienka 
+    <br> NIM : 2241720074';
+   });
+
+   Route::get('/user/{name}', function ($name) {
+    return 'My name is '.$name;
+    });
+
+    Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function
+($Id) {
+ return 'Article Page with ID '.$Id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'My name is '.$name;
+    });
+
