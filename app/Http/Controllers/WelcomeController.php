bac4d96cba7949260690a55ddd70102243321b98
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-02-24 16:22:20 +0700

    add function for view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello() { 
-        return 'Hello World'; 
-        } 
+    public function hello(){ 
+        return('Hello World'); 
+    } 
+
+    public function greeting(){ 
+        return view('blog.hello') ->with('name','Lenka Florienka') ->with('occupation','Engineer'); 
+    } 
 }
